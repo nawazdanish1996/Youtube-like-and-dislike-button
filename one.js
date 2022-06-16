3caa6f31b9d889834fe58f29171ee227c6869d1a
Nawaz Danish <nawazdanish1996@gmail.com>
2022-06-16 20:59:48 +0530

    Youtube like dislike and counter

---
+++ one.js
@@ -0,0 +1,33 @@
+
+// == like or Dislike ==
+
+function like(){
+    document.getElementById('increment').style.color = "blue";
+    document.getElementById('decrement').style.color = "gray";
+}
+
+function dislike(){
+    document.getElementById('increment').style.color = "gray";
+    document.getElementById('decrement').style.color = "blue";
+}
+
+
+// ===== Counter incresement only after click on like button =====
+
+let count = 0;
+
+document.getElementById("increment").onclick = function(){
+    count += 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+// ===== Counter decresement only after click on dislike button =====
+
+document.getElementById("decrement").onclick = function(){
+    count -= 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+
+
+
